848e970a0bd3f5391a45c587e615d6b578c479a0
MindyWei <117993338@qq.com>
2018-02-28 17:22:19 +0800

    removed temporary files.

--- ITE_Castor3_SDK/build/openrtos/risc1_code/project/risc1_code/CMakeFiles/risc1_crt0.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/risc1_crt0"
-  "../../lib/sm32/risc1_crt0.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/risc1_crt0.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
